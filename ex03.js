1133ad337a282709b6665f5b0aa539088a1004d6
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-07-04 10:05:17 -0300

    ex03 ok

--- ex03.js
+++ ex03.js
@@ -3,13 +3,13 @@ let nome = "Ana";
 let vendas = 3000;
 let comissao
 
-if (vendas >= 50000){
+if (vendas >= 50000.00){
     comissao = ((vendas*12)/100);
     
-}else if(vendas >= 30000){
+}else if(vendas >= 30000.00){
     comissao = ((vendas*9.5)/100);
 }else{
     comissao = ((vendas*7)/100);
 }
 
-console.log(`O ${nome} vendeu R$${vendas} e ganhou de comissao R$${comissao.replace()}`)
+console.log(`O ${nome} vendeu ${vendas.toLocaleString("pt-BR", { style: "currency", currency:"BRL"})} e ganhou de comissao ${comissao.toLocaleString("pt-BR",{style: "currency", currency: "BRL"}).replace(".",",")}`)
