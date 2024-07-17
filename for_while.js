1d5225ad252052ce04f61e8bb1af3aabbb00ea96
cheshiry <agnesdacunha02@gmail.com>
2024-07-16 21:05:16 -0300

    ok

---
+++ for_while.js
@@ -0,0 +1,26 @@
+/*for (let cont = 0; cont < 10; cont++) {
+    console.log(cont);
+    console.log("Aumente o contador")
+    
+}
+
+let pontosVida = 0;
+
+for (let i = 0; i < 10; i++) {
+
+    pontosVida += 1;
+    console.log("pontos de vida", pontosVida);
+    
+}
+*/
+let a = 0;
+/*
+while (a < 3) {
+    a++
+    console.log(a)
+}
+*/
+do {
+    a++
+    console.log(a)
+} while (a<50);
---
+++ switchcase.js
@@ -0,0 +1,20 @@
+let fruta = "";
+
+
+switch (fruta) {
+    case "laranja":
+    case "abacaxi":
+    case "uva":
+        console.log("suco");
+        break;
+
+    case "banana":
+    case "maçã":
+    case "mamão":
+        console.log("vitamina");
+        break;
+
+    default:
+        console.log("none");
+        break;
+}
