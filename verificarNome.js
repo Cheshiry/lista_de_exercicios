bc29a71a172e60e146101f82df262ba6fbd107be
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-08-29 15:56:32 -0300

    Create verificarNome.js
    Exercício

---
+++ verificarNome.js
@@ -0,0 +1,9 @@
+
+3) Faça um programa que dado uma lista de nomes imprima os nomes que começem com a letra "V".
+
+const nomes = [veridiana, ag, ariel];
+
+for(var i=0; i<= nomes.length; i++){
+  if (nome[i].leght(0) === "v")
+  console.log(nomes[i]);
+}
