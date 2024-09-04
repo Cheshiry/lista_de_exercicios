cf31b97618e62c5d5931f94398cd6d2a31d584f1
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-09-04 16:01:43 -0300

    Update funcoes03.js
    Ok

--- importacao/funcoes03.js
+++ importacao/funcoes03.js
@@ -1,1 +1,14 @@
+const entradas = [2];
+let i = 0;
 
+function gets(){
+    const valor = entradas[i];
+    i = i + 1;
+    return valor;
+}
+
+function print(texto){
+    console.log(texto);
+}
+
+module.exports = { gets, print };
