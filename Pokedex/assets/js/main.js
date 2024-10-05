6f0718281e942b756cdc22495675025c8d901573
cheshiry <agnesdacunha02@gmail.com>
2024-10-04 21:21:33 -0300

    1º pokemon ok

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,6 +1,6 @@
-const btnTeste = document.getElementById("btn")
-console.log("Testei!!");
+// const btnTeste = document.getElementById("btn")
+// console.log("Testei!!");
 
-btnTeste.addEventListener("click",function(){
-    console.log("Clicou no botâo!");
-})
+// btnTeste.addEventListener("click",function(){
+//     console.log("Clicou no botâo!");
+// })
