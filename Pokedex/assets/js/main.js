ba4cd1a5873bb953e3c06d88b48f3a98a464ff8b
cheshiry <agnesdacunha02@gmail.com>
2024-10-09 21:26:08 -0300

    debuggando

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -4,3 +4,15 @@
 // btnTeste.addEventListener("click",function(){
 //     console.log("Clicou no botâo!");
 // })
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
+fetch(url)
+    .then((response) => response.json())
+    .then (() => )
+    .then((jsonBody) => console.log(jsonBody))
+    debugger
+    .catch((error) => console.error(error))
