7defb15a54afd4b2807597effb2564fd47e06a77
cheshiry <agnesdacunha02@gmail.com>
2024-10-09 22:58:43 -0300

    corrigir

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -9,13 +9,34 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToHtml(pokemon){
+    return `  <ol class="pokemons">
+                <li class="pokemon">
+                    <span class="number">#001</span>
+                    <span class="name">${pokemon.name}</span>
+                    <div class="detail">
+                        <ol class="types">
+                            <li class="type">Grass</li>
+                            <li class="type">Poison</li>
+                        </ol>
+                        <img src="./images/bulbasaur.jpg" alt="${pokemon.name}">
+                    </div>
+                </li>
+            </ol>`
+}
+
+const pokemonList = document.getElementById('pokemonList')
 
 fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then ((pokemonList) => {
-        debugger
-        console.log(pokemonList)
+.then((response) => response.json())
+.then((jsonBody) => jsonBody.results)
+.then ((pokemons) => {
+    for (let i = 0; i < pokemonList.length; i++) {
+        const pokemon = pokemons[i];
+        console.log
+        pokemonList.innerHTML += (convertPokemonToHtml(pokemon))
+            
+        }
     })
     
     .catch((error) => console.error(error))
