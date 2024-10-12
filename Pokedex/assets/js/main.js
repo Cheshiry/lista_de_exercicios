a6964890cbdbab141f61064306662bd19ea3bae3
cheshiry <agnesdacunha02@gmail.com>
2024-10-12 19:54:22 -0300

    ok

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -9,32 +9,32 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonToOl(pokemon){
-    return `<li class="pokemon">
-                <span class="number">#001</span>
-                <span class="name">${pokemon.name}</span>
-                <div class="detail">
-                    <ol class="types">
-                        <li class="type">Grass</li>
-                        <li class="type">Poison</li>
-                    </ol>
-                    <img src="./images/bulbasaur.jpg" alt="${pokemon.name}">
-                </div>
-            </li>`
+function convertPokemonToLi(pokemon){
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">Grass</li>
+                <li class="type">Poison</li>
+            </ol>
+        <img src="./images/bulbasaur.jpg" alt="${pokemon.name}">
+        </div>
+    </li>`
 }
 
 const pokemonList = document.getElementById('pokemonList')
 
 fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then ((pokemonList) => {
-    for (let i = 0; i < pokemonList.length; i++) {
-        const pokemon = pokemons[i];
-        console.log(convertPokemonToOl(pokemon))
-        pokemonList.innerHTML += (convertPokemonToOl(pokemon))
-            
-        }
-    })
-    
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += (convertPokemonToLi(pokemon))
+            }
+        })
+        
     .catch((error) => console.error(error))
