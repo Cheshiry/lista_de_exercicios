9580b5aaf785946b1936c7a53fe32872792ccd29
cheshiry <agnesdacunha02@gmail.com>
2024-12-17 10:46:12 -0300

    modal 17.12

--- Modal/Pokedex2/assets/js/modal.js
+++ Modal/Pokedex2/assets/js/modal.js
@@ -1,4 +1,4 @@
-const pokemonList = document.getElementById('pokemonList');
+const pokemonList = document.getElementById('modal');
     data.forEach(item => {
         const li = document.createElement('li');
         li.textContent = item.name;
