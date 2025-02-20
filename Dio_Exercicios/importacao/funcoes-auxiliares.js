542a89b26529c0c4f2da7370092abed0a482088a
cheshiry <agnesdacunha02@gmail.com>
2025-02-20 14:24:40 -0300

    ok

--- Dio_Exercicios/importacao/funcoes-auxiliares.js
+++ Dio_Exercicios/importacao/funcoes-auxiliares.js
@@ -5,3 +5,5 @@ function gets(params) {
 function print(texto) {
     console.log(texto)
 }
+
+module.exports = { gets, print };
---
+++ Dio_Exercicios/importacao02/maiorNumero.js
@@ -0,0 +1,13 @@
+//nao esta completo
+
+const {gets, print} = require('./funcoes03');
+
+let maiorValorEncontrado = 0;
+const quantida = gets();
+
+const menorValor = Math.min(quantida);
+const maior = Math.max(quantida)
+
+print(maior);
+print(menorValor);
+
