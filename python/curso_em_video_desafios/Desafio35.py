d9ad3e705ba279882d3740f731cdaf90ea52e66f
cheshiry <agnesdacunha02@gmail.com>
2025-04-12 21:46:18 -0300

    ok

--- python/curso_em_video_desafios/Desafio35.py
+++ python/curso_em_video_desafios/Desafio35.py
@@ -6,10 +6,6 @@ r1 = int(input("Digite um segmento d3 reta: "))
 r2 = int(input("Digite outro segmento de reta: "))
 r3 = int(input("Digite mais um segmento  de reta: "))
 
-oposto1 = r1 + r2
-oposto2 = r1 + r3
-oposto3 = r2 + r3
-
 if r1 < r2 + r3 and r2 < r1 +r3 and r3 < r1 + r2:
     print("É um triângulo")
 
