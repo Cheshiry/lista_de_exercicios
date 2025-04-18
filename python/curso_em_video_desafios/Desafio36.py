1ee3ccde29d0c8cfc4c604c58eadeee709c10975
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2025-04-18 17:38:27 -0300

    Update Desafio36.py
    Ok

--- python/curso_em_video_desafios/Desafio36.py
+++ python/curso_em_video_desafios/Desafio36.py
@@ -5,3 +5,18 @@ print("\033[1;97;46m Descobrir se pode financiar sua casa\n")
 valor = float(input("Digite o valor da casa: "))
 quanto_tempo = int(input("Em quantos anos planeja quitar a o financiamento da casa: "))
 
+print("-=" * 20)
+print("\033[1;36;46mAnalisador de financiamento \033[m")
+print("-=" * 20)
+
+anos = int(input("Digite em quanto anos quer pagar a casa: "))
+salario = float(input("Digite seu salário: "))
+valor_casa = float(input("Digite o valor da casa: "))
+
+valor_mensal = valor_casa / (anos * 12)
+salario_30p = (salario * 30) / 100
+
+if valor_mensal <= salario_30p:
+    print(f"Consórcio aprovado valor mensal a ser pago R${valor_mensal:.2f} por {anos} anos")
+else:
+    print("Consórcio negado ")
