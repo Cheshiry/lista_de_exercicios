4ad8e3da3f75b97ffaa33784f5357345f4d0ccfe
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2025-04-27 20:19:23 -0300

    Update Desafio41.py
    Ok now

--- python/curso_em_video_desafios/Desafio41.py
+++ python/curso_em_video_desafios/Desafio41.py
@@ -1,16 +1,30 @@
-<<<<<<< HEAD
+from datetime import date
+
 print("\033[1;95m*" * 50)
 print("Classificação Nacional de atletas: ")
 
-idade = int(input("Digite a idade do atleta: "))
+ano_nasc = int(input("Digite o ano de nascimento do atleta: "))
+
+ano = date.today().year
+
+idade = ano - ano_nasc
 
 if idade <= 9:
-    print("Mirim")
+    print(f"Quem nasceu em {ano_nasc} é")
+    print("Mirim com ",idade,"anos")
+    
 elif idade <= 14:
-    print("Infantil")
+    print(f"Quem nasceu em {ano_nasc} é")
+    print("Infantil", idade,"anos")
+    
 elif idade <= 19:
-    print("Junior")
-elif idade <= 20:
-    print("Sênior")
+    print(f"Quem nasceu em {ano_nasc} é")
+    print("Junior", idade,"anos")
+    
+elif idade < 20:
+    print(f"Quem nasceu em {ano_nasc} é")
+    print("Sênior", idade,"anos")
+    
 else:
-    print("Master")
+    print(f"Quem nasceu em {ano_nasc} é")
+    print("Master", idade,"anos")
