718e7a9ff36f7f49f34956263c21ee836b43cb35
cheshiry <agnesdacunha02@gmail.com>
2025-05-07 20:06:49 -0300

    ok

--- python/curso_em_video_desafios/Desafio56.py
+++ python/curso_em_video_desafios/Desafio56.py
@@ -2,23 +2,30 @@ somaidade = 0
 maioridade = 0
 mediaidade = 0
 nomevelho = ""
+menoridade = 0
+
 
 for p in range(1, 5):
-    print(f"{p}° pessoa")
+    print(f"----------{p}° pessoa----------")
     nome = str((input("Nome: "))).strip()
     idade = int((input("idade: ")))
     sexo = str((input("Sexo [M/F]: "))).strip()
-    print("*" * 40)
+    
     somaidade += idade
     
     if p == 1 and sexo in "Mm":
         maioridade = idade
         nomevelho = nome
-    if sexo == "Mm" and idade > maioridade:
+    
+    if sexo in "Mm" and idade > maioridade:
         maioridade = idade
         nomevelho = nome
+        
+    if sexo in "Ff" and idade < 20:
+        menoridade += 1
         
 mediaidade = somaidade/ 4
 
 print(f"A media das idade é {mediaidade}.")
 print(f"O homem mais velho tem {maioridade} e seu nome é {nomevelho}")
+print(f"Tem {menoridade} mulheres com menos de 20 anos.")
