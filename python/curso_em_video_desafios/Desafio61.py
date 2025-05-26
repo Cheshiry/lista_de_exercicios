ee5f20c9c351e2e704ca03c433e80f022fa996f8
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2025-05-26 15:48:56 -0300

    Update Desafio61.py
    Ok

--- python/curso_em_video_desafios/Desafio61.py
+++ python/curso_em_video_desafios/Desafio61.py
@@ -1,1 +1,12 @@
+primeiro = int(input("1º termo: " ))
+razao = int(input("Razão: " ))
+decimo = primeiro + (10 - 1) * razao
+i = 1
 
+while i <= 10:
+          
+    print(primeiro, end = " → " )
+    primeiro += razao 
+    i += 1
+    
+print("Fim")
