ec0ae26e03ae1b2f54c2e3384fe0a11c55273ed5
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2025-06-18 09:20:48 -0300

    Update Desafio64.py
    Corrigido

--- python/curso_em_video_desafios/Desafio64.py
+++ python/curso_em_video_desafios/Desafio64.py
@@ -1,9 +1,13 @@
 i = 0
-num = 0
-
+soma
+num = int(input("Digite um número inteiro diferente de 999:\n"))
 while num != 999:
+    
     print("\033[1;97;26m=-"*30)
+    soma += num
+    i+=1
+    
     num = int(input("Digite um número inteiro diferente de 999:\n"))
-    i += num
+    
 print("=-"*30)
 print(f"A soma dos números digitados foi : {i-999}")
