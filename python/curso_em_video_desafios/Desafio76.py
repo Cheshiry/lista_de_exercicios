c946f163288357f69ccdebf2bb1d0889cfc290c9
Cheshiry <agnesdacunha02@gmail.com>
2025-07-22 06:17:53 -0300

    ok

--- python/curso_em_video_desafios/Desafio76.py
+++ python/curso_em_video_desafios/Desafio76.py
@@ -1,9 +1,19 @@
-produtos = ("ração", 190,"Areia", 20)
+produtos = ("ração", 190,
+            "Areia", 20,
+            "Comedouro", 10,
+            "Bebedouro",10,
+            "Sachê",8)
 i = 0
-print("Loja DaVilla")
-print("-="*30)
-print(f"{'Loja':^40}")
-for n in produtos:
-    print(produtos[i], end= " ")
-    i += 1
-print("-="*30)
+
+print(f"{'Loja DaVilla':^40}")
+print("-"*40)
+print(f"{'Listgem de preços':^40}")
+print("-"*40)
+
+for item in range(0,len(produtos)):
+    if item % 2 == 0:
+        print(f"{produtos[item]:.<30}" , end="")
+    else:
+        print(f"R${produtos[item]:>7.2f}")
+
+print("-"*40)
